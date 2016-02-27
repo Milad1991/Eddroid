d1e147081149cb57259d6f89bc784e1c219532f4
Milad Zolnoor <miladzolnoor@gmail.com>
2016-02-27 17:37:30 -0500

    no errors and map works

--- app/assets/javascripts/Menu.js
+++ app/assets/javascripts/Menu.js
@@ -11,16 +11,16 @@ Eddroid.Menu.prototype = {
 
   create: function() {
     backgroundPic = this.add.image(0, 0, 'premap');
-    Eddroidlogo = this.add.image(90, 40, 'logo');
+    Eddroidlogo = this.add.image(90, 50, 'logo');
     startPrompt = this.add.text(120, 400, "Space Bar To Begin The Experience", { font: "35px Arial", fill: "#fff", align: "center" });
     this.enabled = true;
     spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
-    spaceKey.onDown.add(this.start, this);
-    console.log('Daaaammmmnnnnn, Daniel');
+    spaceKey.onDown.add(this.startGame, this);
+    // console.log('Daaaammmmnnnnn, Daniel');
 
   },
-  start: function() {
+  startGame: function() {
       this.state.start('Main');
   }
 };
