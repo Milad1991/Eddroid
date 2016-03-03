ab47b8a7a3a337bd6e914ae542cf5d7db3e8ac7a
Matt Kellough <mrk10a@gmail.com>
2016-03-03 17:23:59 -0500

    Enemies Collision animation working

--- app/assets/javascripts/Menu.js
+++ app/assets/javascripts/Menu.js
@@ -12,7 +12,7 @@ Eddroid.Menu.prototype = {
   create: function() {
     backgroundPic = this.add.image(0, 0, 'premap');
     Eddroidlogo = this.add.image(90, 50, 'logo');
-    startPrompt = this.add.text(120, 400, "Space Bar To Begin The Experience", { font: "35px Arial", fill: "#fff", align: "center" });
+    // startPrompt = this.add.text(120, 400, "Space Bar To Begin The Experience", { font: "35px Arial", fill: "#fff", align: "center" });
     this.enabled = true;
     spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
